62322b9a9b7a74369d2f82a510f712f86ade64dd
Suresh Prasanna <sureshprasanna70@gmail.com>
2018-07-14 18:05:40 +0530

    initial commit

---
+++ lib/msg91.rb
@@ -0,0 +1,5 @@
+require "msg91/version"
+
+module Msg91
+  # Your code goes here...
+end
---
+++ spec/msg91_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+RSpec.describe Msg91 do
+  it "has a version number" do
+    expect(Msg91::VERSION).not_to be nil
+  end
+end
