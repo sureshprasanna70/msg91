11a96f8d044e68fc673d5167fad3ea25cc61af25
Suresh Prasanna <sureshprasanna70@gmail.com>
2018-07-16 19:27:02 +0530

    rspec for configure

--- spec/msg91_spec.rb
+++ spec/msg91_spec.rb
@@ -1,7 +1,22 @@
 require "spec_helper"
 
 RSpec.describe Msg91 do
-  it "has a version number" do
-    expect(Msg91::VERSION).not_to be nil
+  describe "#configure" do
+
+    before(:each) do
+      Msg91.configure do |config|
+        config.auth_key = ENV['MSG91_AUTH_KEY'] 
+      end
+    end
+
+
+    it "has a version number" do
+      expect(Msg91::VERSION).not_to be nil
+    end
+
+    it 'has a auth_key' do
+      auth_key = Msg91.configuration.auth_key
+      expect(auth_key).to eq(ENV['MSG91_AUTH_KEY'])
+    end
   end
 end
