c7f63f8479f2da01c9638bbe91af741fc1691d48
Suresh Prasanna <suresh@cybrilla.com>
2021-10-31 20:51:32 +0530

    update readme url

--- lib/generators/msg91/install_generator.rb
+++ lib/generators/msg91/install_generator.rb
@@ -6,7 +6,7 @@ module Msg91
       def copy_initializer
         template 'msg91_initializer.rb', 'config/initializers/msg91.rb'
 
-        puts 'Install complete! Please visit https://github.com/digiryte/msg91 for documentation'
+        puts 'Install complete! Please visit https://github.com/sureshprasanna70/msg91 for documentation'
       end
     end
   end
